4df5868fc8ac64499638a93cd52875e3606a1757
HrithiK_MrXHam1 <46870401+hrithiks2019@users.noreply.github.com>
2020-03-30 01:16:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,36 +11,10 @@ from math import log10
 from bs4 import BeautifulSoup
 import numpy as np
 
-teleportation = 0.05
-target_delta = 0.04
-
-stop_words = [
-    'a', 'also', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'do',
-    'for', 'have', 'is', 'in', 'it', 'of', 'or', 'see', 'so',
-    'that', 'the', 'this', 'to', 'we'
-]
-
-
-def main():
-    args = get_args()
-    # Computing
-    pages = crawl(args.url)
-    ranks = page_rank(pages)
-    rank = best_rank(ranks, pages)
-    N = len(pages)
-    index = create_index(pages)
-    weighted_index = weight_index(index, N)
-    norm_index = normalize_index(weighted_index)
-
-    # Print results
-    print()
-    print('Number of pages:', len(pages))
-    print('Terms in index:', len(index))
-    print('Interations for PageRank:', len(ranks))
-    print()
-    print_combined_search(norm_index, N, rank, args.query)
-
-
+tpx,tdx,stop_words = 0.05,0.04,['a', 'also', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'do',
+					'for', 'have', 'is', 'in', 'it', 'of', 'or', 'see', 'so',
+					'that', 'the', 'this', 'to', 'we']
+  
 def get_args():
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -58,9 +32,6 @@ def get_args():
         help='At least one seed url for the crawler to start from'
     )
     return parser.parse_args()
-
-
-# Crawler
 
 
 def crawl(urls, _frontier={}, _bases=None):
@@ -94,11 +65,6 @@ def download(url):
 
 
 def parse(html, url, bases):
-    '''
-    Takes an html string and a url as arguments.
-
-    Returns a tuple (url, content, links) parsed from the html.
-    '''
     soup = BeautifulSoup(html, 'lxml')
 
     content = soup.body.get_text().strip()
@@ -109,12 +75,6 @@ def parse(html, url, bases):
 
 
 def page_rank(pages):
-    '''
-    Returns a matrix with documents as columns
-    and values for each round as rows.
-
-    Number of rows depends on how long it takes to reach the target_delta.
-    '''
     N = len(pages)
     transition_matrix = create_transition_matrix(pages)
     ranks_in_steps = [[1 / N] * N]
@@ -122,18 +82,11 @@ def page_rank(pages):
         possibilities = ranks_in_steps[-1] * transition_matrix
         delta = get_delta(possibilities, ranks_in_steps[-1])
         ranks_in_steps.append(np.squeeze(np.asarray(possibilities)))
-        if delta <= target_delta:
+        if delta <= tdx:
             return ranks_in_steps
 
 
 def create_transition_matrix(pages):
-    '''
-    Returns a matrix with document urls as rows
-    and document links as columns.
-
-    Each cell contains the propability for a document
-    to transition to a link.
-    '''
     links = get_links(pages)
     urls = get_urls(pages)
     N = len(pages)
@@ -151,7 +104,7 @@ def weight_link(N, url, links):
 
 
 def teleport(N, m):
-    return m * (1 - teleportation) + teleportation / N
+    return m * (1 - tpx) + tpx / N
 
 
 def get_delta(a, b):
@@ -167,22 +120,12 @@ def get_links(pages):
 
 
 def best_rank(ranks, pages):
-    '''
-    Returns a dict with document urls as keys
-    and their ranks as values.
-    '''
     return dict(zip(get_urls(pages), ranks[-1]))
 
 
 # Index
 
 def create_index(pages):
-    '''
-    Returns the index as a dict with terms as keys
-    and lists tuples(url, count) as values.
-
-    Count says how many times the term occured in the document.
-    '''
     index = defaultdict(list)
     for url, content, links in pages:
         counts = count_terms(content)
@@ -192,12 +135,6 @@ def create_index(pages):
 
 
 def count_terms(content):
-    '''
-    content is a text string.
-
-    Returns a Counter with terms as keys
-    and their occurence as values.
-    '''
     return Counter(get_terms(content))
 
 
@@ -205,21 +142,11 @@ normalize = re.compile('[^a-z0-9]+')
 
 
 def get_terms(s):
-    '''
-    Get a list of terms from a string.
-    Terms are lower case and all special characters are removed.
-    '''
     normalized = [normalize.sub('', t.lower()) for t in s.split()]
     return [t for t in normalized if t not in stop_words]
 
 
 def weight_index(index, N):
-    '''
-    Takes an index as first argument
-    and the total number of documents as second argument.
-
-    Returns a new index with tf_idf weights instead of simple counts.
-    '''
     weighted_index = defaultdict(list)
     for term, docs in index.items():
         df = len(docs)
@@ -242,11 +169,6 @@ def idf(N, df):
 
 
 def normalize_index(index):
-    '''
-    Takes an index as argument.
-
-    Returns a new index with normalized weights.
-    '''
     lengths = doc_lengths(index)
     norm_index = defaultdict(list)
     for term, docs in index.items():
@@ -282,11 +204,21 @@ def combined_search(index, N, rank, query):
     return sorted(combined, key=lambda x: x[1], reverse=True)
 
 
-def print_combined_search(index, N, rank, query):
+def pcs(index, N, rank, query):
     print('Search results for "%s":' % (query))
     for url, score in combined_search(index, N, rank, query):
         print('%.6f  %s' % (score, url))
 
 
-if __name__ == "__main__":
-    main()
+
+args = get_args()
+pages = crawl(args.url)
+ranks = page_rank(pages)
+rank = best_rank(ranks, pages)
+N = len(pages)
+index = create_index(pages)
+weighted_index = weight_index(index, N)
+norm_index = normalize_index(weighted_index)
+print('No.of.pages:', len(pages))
+print('PageRank:', len(ranks))
+pcs(norm_index, N, rank, args.query)
